3692798bd9900851aadeecc29fe3ce663ebc7331
Soham-711 <sohambiswas711@gmail.com>
2025-04-24 11:03:45 +0530

    Hand over to saikat

--- Backend/Controllers/resumeController.js
+++ Backend/Controllers/resumeController.js
@@ -1,9 +1,10 @@
 const Resume = require("../Models/resumeModel");
 require('dotenv').config()
 const axios = require('axios')
+const cloudinary = require('../utils/cloudinary')
 
 const apiKey = process.env.apiKey;
-const resumeUrl = 'public\\temp\\sample-resumes.docx';
+const resumeUrl = 'http://res.cloudinary.com/dybr6vuh9/image/upload/v1745472579/resumes/qql8lf3e9fgmkkfwlpbs.pdf';
 
 const uploadResume = async (req, res) => {
   try {
@@ -13,13 +14,20 @@ const uploadResume = async (req, res) => {
     if (!file) {
       return res.status(400).json({ message: "No file uploaded", success: false });
     }
-    console.log(file);
     
+       // Upload to Cloudinary (make sure your Cloudinary config is set)
+       const result = await cloudinary.uploader.upload(file.path, {
+        resource_type: "auto", // Accepts pdf, docx, etc.
+        folder: "resumes"
+      });
+  
+      console.log("Cloudinary upload result:", result);
+  
 
    const newResume = new Resume({
     fileName:file.originalname,
     cloudinaryUrl:file.path,
-    publicId:file.de,
+    publicId:file.publicId,
     jobRole
    })
   await newResume.save();
@@ -39,14 +47,18 @@ const uploadResume = async (req, res) => {
   }
 };
 
-const apiResumeParserCall = async ()=>{
+const apiResumeParserCall = async (req,res)=>{
   try {
     const response = await axios.get(`https://api.apilayer.com/resume_parser/url?url=${resumeUrl}`, {
       headers: {
         'apikey': apiKey
       }
     });
-    console.log(response);
+    console.log(response.data);
+    return res.status(200).json({
+      message:"Successfully",
+      responce:response.data
+    })
   } catch (error) {
     console.error('Error:', error);
   }
